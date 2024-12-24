447ea0d3688dfe9ef5055788478c11b322ccff87
Nour El Hoda El Kaouti <132302883+KJXFPIGJDMBDPT9@users.noreply.github.com>
2024-12-24 09:43:59 +0100

    Update get_nessus_ip.sh

--- nessus/get_nessus_ip.sh
+++ nessus/get_nessus_ip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-Récupérer l'adresse IP du container Nessus
 NESSUS_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nessus)
 if [ -z "$NESSUS_IP" ]; then
-  echo "Erreur : Impossible de récupérer l'IP du container Nessus."
+  echo "Erreur : Impossible de récupérer l'IP du conteneur Nessus."
   exit 1
 fi
 echo "$NESSUS_IP" > nessus_ip.txt
+echo "Adresse IP de Nessus sauvegardée : $NESSUS_IP"
